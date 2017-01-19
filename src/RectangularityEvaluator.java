4f9795e40461857afa7191e1b4072d6e11f946d7
Joel Manning <stopwatchj@gmail.com>
2017-01-18 16:42:26 -0800

    Rectangle

---
+++ src/RectangularityEvaluator.java
@@ -0,0 +1,27 @@
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.Rect;
+import org.opencv.imgproc.Imgproc;
+
+/**
+ * 
+ */
+
+/**
+ * @author joelmanning
+ *
+ */
+public class RectangularityEvaluator extends Evaluator {
+	
+	/* (non-Javadoc)
+	 * @see Evaluator#calculateError(org.opencv.core.MatOfPoint, org.opencv.core.MatOfPoint)
+	 */
+	@Override
+	public double calculateError(MatOfPoint a, MatOfPoint b) {
+		Rect ar = Imgproc.boundingRect(a);
+		Rect br = Imgproc.boundingRect(b);
+		double aa = ar.area();
+		double ba = ar.area();
+		return Math.abs(aa - Imgproc.contourArea(a))/aa + Math.abs(ba - Imgproc.contourArea(b))/ba;
+	}
+	
+}
